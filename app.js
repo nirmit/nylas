ab3fc9c45d73a339ac22701e7b75f35f0a7c292f
Manoj <manoj@anilabsinc.com>
2017-06-08 15:30:16 +0530

    added body-parser and nylas module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var dotenv = require('dotenv/config')
+var dotenv = require('dotenv/config');
 console.log(process.env.API_ID)
 var express = require( 'express' );
 var http = require( 'http' );
@@ -10,7 +10,6 @@ app.set( 'port', process.env.PORT || 4000 );
 var passport = require('passport');
 var flash = require('connect-flash');
 var url = process.env.MONGODB_URI;
-
 
 mongoose.connect(url,function(err){
 	if(err){
@@ -29,8 +28,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-
-
 app.use(morgan('dev')); 
 app.use(cookieParser()); 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -46,49 +43,8 @@ global.Nylas = require('nylas').config({
    appSecret: process.env.API_SECRET
 });
 
-
 require('./app/routes.js')(app,passport);
-
 
 http.createServer( app ).listen( app.get( 'port' ), function (){
   console.log( 'Express server listening on port ' + app.get( 'port' ));
 });
-
-
-
-
-
-
-
-
-
-/*
-var dotenv = require('dotenv/config')
-console.log(process.env.API_ID)
-var url = process.env.MONGODB_URI;
-mongoose.connect(url,function(err){
-	if(err){
-		console.log(err);
-
-	} else{
-		console.log("connected to mongodb");
-	}
-});
-global.Nylas = require('nylas').config({
-   appId: process.env.API_ID,
-   appSecret: process.env.API_SECRET
-});
-
-create file .env in ravinode
-
-
-API_ID = 68uz44xs3kiqhmpxxw08n5yi3
-
-API_SECRET = 6tk575tuvjpad651s02k23n7b
-
-MONGODB_URI = mongodb://tango:tango123@localhost/royal
-
-*/ 
-
-
-
